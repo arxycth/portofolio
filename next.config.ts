e0497bca0890143d0a157120f4e7fe57ac3fc6f0
arxycth <dimasadiluhur@gmail.com>
2025-06-22 15:25:52 +0700

    add whitelist domain for icon

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  images: {
+    domains: ["skillicons.dev"], // ✅ Tambahkan ini
+  },
 }
 
 export default nextConfig;
